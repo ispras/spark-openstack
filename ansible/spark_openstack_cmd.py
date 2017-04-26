3229882308ee4f8a11b2058761125759e523137f
Oleg Borisenko <al@somestuff.ru>
2017-04-26 13:45:04 +0300

    Spark 2.1.0

--- ansible/spark_openstack_cmd.py
+++ ansible/spark_openstack_cmd.py
@@ -9,6 +9,7 @@ import os
 
 spark_versions = \
     {
+        "2.1.0": {"hadoop_versions": ["2.3", "2.4", "2.6", "2.7"]},
         "2.0.2": {"hadoop_versions": ["2.3", "2.4", "2.6", "2.7"]},
         "2.0.1": {"hadoop_versions": ["2.3", "2.4", "2.6", "2.7"]},
         "2.0.0": {"hadoop_versions": ["2.3", "2.4", "2.6", "2.7"]},
